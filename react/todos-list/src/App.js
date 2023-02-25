26fd904cc16d4634a5d5118066f927825e12e4ac
Purushottam Nawale <nawalepb19.mech@coep.ac.in>
2023-02-25 18:26:04 +0530

    commit

---
+++ react/todos-list/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let myVariable=345;
+  return (
+    <>
+    <h3>My App</h3>
+    <p>My app works</p>
+    </>
+  );
+}
+
+// className is JSX i.e. JavaScript Sytax Extention.
+
+export default App;
